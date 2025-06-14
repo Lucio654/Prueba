89f95df3deed1f35f2b322154139392516adb3ad
Lucio <luc_654@hotmail.com>
2025-06-14 17:49:52 -0400

    se resolvio el ejericio 5-e14 cuenta regresiva

---
+++ ejemplos/e14-contador-regresivo/Program.cs
@@ -0,0 +1,13 @@
+﻿//  5. Contador regresivo
+//Mostrar una cuenta regresiva del 10 al 1 usando while.
+
+
+int leerPorTeclado = 10;
+while (leerPorTeclado > 0)
+{
+    Console.WriteLine(leerPorTeclado);
+    leerPorTeclado--;
+}
+Console.WriteLine();
+Console.Write("Cuenta regresiva terminada");
+
