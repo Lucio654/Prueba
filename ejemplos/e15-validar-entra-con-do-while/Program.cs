67c978a84a78dcdd7f4ce39813f9c089d80a053e
Lucio <luc_654@hotmail.com>
2025-06-14 18:38:31 -0400

    Se realizo el ejericio 6: Validar entrada con Do While

---
+++ ejemplos/e15-validar-entra-con-do-while/Program.cs
@@ -0,0 +1,27 @@
+﻿// 6. Validar entrada con do-while
+//Pedir al usuario que introduzca un número positivo y seguir pidiendo hasta que lo haga correctamente.
+
+    int numero;
+
+
+do
+{
+    Console.Clear();
+    Console.Write("Introduzca un numero positivo: ");
+    while (!int.TryParse(Console.ReadLine(), out numero))
+    {
+    Console.WriteLine("Error: Debe ingresar un número válido.");
+    Console.Write("Introduzca un número positivo: ");
+    }
+    if (numero <= 0)
+    {
+    Console.WriteLine("Ingrese un valor positivo");
+    Console.WriteLine("Presiona una tecla para continuar...");
+    Console.ReadKey();
+    }
+    
+
+} while (numero <= 0);
+Console.Write("se ingreso correctamente");
+
+     
