01e4d3d39b0aa128574199636a3a732305f3c101
Lucio <luc_654@hotmail.com>
2025-06-14 22:01:24 -0400

    se realizo el juego de la adivinanza

---
+++ ejemplos/e19-adivina-el-numero/Program.cs
@@ -0,0 +1,40 @@
+﻿// 10. Adivina el número (juego)
+//El sistema genera un número aleatorio del 1 al 50.
+//El usuario intenta adivinarlo.
+//Usar do-while hasta que acierte, indicando si es mayor o menor.
+
+using System.Timers;
+
+Random random = new Random();
+int numeroAleatorio = random.Next(1, 51);
+int jugador;
+
+
+do
+{
+    Console.Write("Ingrese un numero");
+
+    if (int.TryParse(Console.ReadLine(), out jugador))
+    {
+        if (jugador == numeroAleatorio)
+        {
+            Console.WriteLine("usted adivino el numero");
+        }
+        else if (jugador < numeroAleatorio)
+        {
+            Console.WriteLine("El numero es Mayor, intentelo de nuevo");
+        }
+        else
+        {
+            Console.WriteLine("El numero es menor, intentelo de nuevo");
+        }
+
+
+    }
+    else
+    {
+        Console.WriteLine("ingrese valores numericos");
+    }
+} while (jugador != numeroAleatorio);
+
+  
