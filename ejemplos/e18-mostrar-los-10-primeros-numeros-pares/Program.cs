51526b380476a1bfaacb513a653197f90bc9b8d7
Lucio <luc_654@hotmail.com>
2025-06-14 21:39:46 -0400

    solucion al ejercicio 9 - Mostrar los primeros 10 numero pares

---
+++ ejemplos/e18-mostrar-los-10-primeros-numeros-pares/Program.cs
@@ -0,0 +1,13 @@
+﻿// 9. Mostrar los primeros 10 números pares
+//Mostrar en pantalla los primeros 10 números pares usando while.
+int numero=1;
+int aux = 0;
+while (aux < 10)
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine(numero);
+        aux++;
+    }
+    numero++;
+}
